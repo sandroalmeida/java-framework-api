5d2502fcb071c19062fe1d8f185818b29ea5e050
Sandro Almeida <sandrodotbuild@gmail.com>
2024-09-23 17:41:22 -0600

    adding basic structure

---
+++ src/main/java/com/sandrocorrea/java_framework_api/JavaFrameworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.sandrocorrea.java_framework_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFrameworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFrameworkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandrocorrea/java_framework_api/JavaFrameworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandrocorrea.java_framework_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFrameworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
