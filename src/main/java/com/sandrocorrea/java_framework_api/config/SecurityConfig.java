3ca6364fb24845d9bd8ccfda6fea94d44e6bf6ac
Sandro Almeida <sandrodotbuild@gmail.com>
2024-09-24 21:58:14 -0600

    adding google login

---
+++ src/main/java/com/sandrocorrea/java_framework_api/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.sandrocorrea.java_framework_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) ->
+                        requests
+                                .requestMatchers("/login", "/oauth2/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/", true)
+                )
+                .logout(logout ->
+                        logout
+                                .logoutSuccessUrl("/login").permitAll()
+                );
+
+        return http.build();
+    }
+}
