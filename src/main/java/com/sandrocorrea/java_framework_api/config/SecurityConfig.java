d5651eff667a9971c81a120c2d2a6419fdbb1b82
Sandro Almeida <sandrodotbuild@gmail.com>
2024-09-26 22:21:21 -0600

    changing SecurityConfig to handle github login

--- src/main/java/com/sandrocorrea/java_framework_api/config/SecurityConfig.java
+++ src/main/java/com/sandrocorrea/java_framework_api/config/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.sandrocorrea.java_framework_api.config;
 
+import com.sandrocorrea.java_framework_api.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -17,9 +22,30 @@ public class SecurityConfig {
                     .permitAll()
                     .anyRequest()
                     .authenticated())
-        .oauth2Login(oauth2Login -> oauth2Login.loginPage("/login").defaultSuccessUrl("/", true))
+        .oauth2Login(
+            oauth2Login ->
+                oauth2Login
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/", true)
+                    .userInfoEndpoint(
+                        userInfo -> {
+                          userInfo.oidcUserService(this.oidcUserService()); // For Google (OIDC)
+                          userInfo.userService(this.oAuth2UserService()); // For GitHub (OAuth2)
+                        }))
         .logout(logout -> logout.logoutSuccessUrl("/login").permitAll());
 
     return http.build();
+  }
+
+  // Google OIDC user processing
+  @Bean
+  public OidcUserService oidcUserService() {
+    return new OidcUserService();
+  }
+
+  // GitHub OAuth2 user processing
+  @Bean
+  public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+    return new CustomOAuth2UserService();
   }
 }
