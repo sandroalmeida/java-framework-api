870dba6b13d16ac31c82bd09ae4ea2c446bb8479
Sandro Almeida <sandrodotbuild@gmail.com>
2024-09-28 19:27:14 -0600

    implementing linkedin login changes

--- src/main/java/com/sandrocorrea/java_framework_api/config/SecurityConfig.java
+++ src/main/java/com/sandrocorrea/java_framework_api/config/SecurityConfig.java
@@ -4,11 +4,19 @@ import com.sandrocorrea.java_framework_api.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @EnableWebSecurity
 public class SecurityConfig {
@@ -16,30 +24,30 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(
-                    (requests) ->
-                            requests
-                                    .requestMatchers("/login", "/oauth2/**")
-                                    .permitAll()
-                                    .anyRequest()
-                                    .authenticated())
-            .oauth2Login(
-                    oauth2Login ->
-                            oauth2Login
-                                    .loginPage("/login")
-                                    .defaultSuccessUrl("/", true)
-                                    .userInfoEndpoint(
-                                            userInfo -> {
-                                              // Handle OpenID Connect (Google and LinkedIn)
-                                              userInfo.oidcUserService(this.oidcUserService());
-                                              // Handle OAuth2 (GitHub)
-                                              userInfo.userService(this.oAuth2UserService());
-                                            }))
-            .logout(logout -> logout.logoutSuccessUrl("/login").permitAll());
+            (requests) ->
+                requests
+                    .requestMatchers("/login", "/oauth2/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .oauth2Login(
+            oauth2Login ->
+                oauth2Login
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/", true)
+                    .userInfoEndpoint(
+                        userInfo -> {
+                          // Handle OpenID Connect (Google)
+                          userInfo.oidcUserService(this.oidcUserService());
+                          // Handle OAuth2 (GitHub)
+                          userInfo.userService(this.oAuth2UserService());
+                        }))
+        .logout(logout -> logout.logoutSuccessUrl("/login").permitAll());
 
     return http.build();
   }
 
-  // Google and LinkedIn OIDC user processing
+  // Google OIDC user processing
   @Bean
   public OidcUserService oidcUserService() {
     return new OidcUserService();
@@ -48,6 +56,33 @@ public class SecurityConfig {
   // GitHub OAuth2 user processing
   @Bean
   public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
-    return new CustomOAuth2UserService();  // Assuming CustomOAuth2UserService handles GitHub users
+    return new CustomOAuth2UserService(); // Assuming CustomOAuth2UserService handles GitHub users
+  }
+
+  @Bean
+  public OAuth2AuthorizedClientManager authorizedClientManager(
+      ClientRegistrationRepository clientRegistrationRepository,
+      OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+    OAuth2AuthorizedClientProvider authorizedClientProvider =
+        OAuth2AuthorizedClientProviderBuilder.builder()
+            .authorizationCode()
+            .refreshToken()
+            .clientCredentials()
+            .build();
+
+    DefaultOAuth2AuthorizedClientManager authorizedClientManager =
+        new DefaultOAuth2AuthorizedClientManager(
+            clientRegistrationRepository, authorizedClientRepository);
+    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+    return authorizedClientManager;
+  }
+
+  @Bean
+  public WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+    ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
+        new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+    return WebClient.builder().apply(oauth2Client.oauth2Configuration()).build();
   }
 }
