53122d99e3bc5a1410e74398c042992dcc059d29
Sandro Almeida <sandrodotbuild@gmail.com>
2024-10-08 18:39:11 -0600

    implementing profile saving

--- src/main/java/com/sandrocorrea/java_framework_api/controller/ProfileController.java
+++ src/main/java/com/sandrocorrea/java_framework_api/controller/ProfileController.java
@@ -1,18 +1,42 @@
 package com.sandrocorrea.java_framework_api.controller;
 
+import com.sandrocorrea.java_framework_api.model.Profile;
+import com.sandrocorrea.java_framework_api.service.ProfileService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProfileController {
 
-    @GetMapping("/add-profile")
-    public String showAddProfilePage() {
-        return "add-profile";
-    }
+  private final ProfileService profileService;
 
-    @GetMapping("view-profiles")
-    public String showViewProfilesPage() {
-        return "view-profiles";
+  public ProfileController(ProfileService profileService) {
+    this.profileService = profileService;
+  }
+
+  @GetMapping("/add-profile")
+  public String showAddProfilePage(Model model) {
+    model.addAttribute("profile", new Profile());
+    return "add-profile";
+  }
+
+  @PostMapping("/save-profile")
+  public String saveProfile(@ModelAttribute Profile profile, Model model, BindingResult result) {
+    if (result.hasErrors()) {
+      model.addAttribute("message", "Please correct the errors in the form");
+      return "add-profile";  // Return to the form if there are validation errors
     }
+    profileService.saveProfile(profile);
+    model.addAttribute("message", "Profile saved successfully!");
+    return "redirect:/view-profiles";
+  }
+
+  @GetMapping("view-profiles")
+  public String showViewProfilesPage() {
+    return "view-profiles";
+  }
 }
